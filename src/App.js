42e2b6be960826b92345d8ec0303452947a07c50
Samarth Malhotra <malhotra.samarth@hotmail.com>
2023-02-19 13:18:39 +0530

    added dark mode

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
 import "./App.css";
-import { BsFillMoonStarsFill } from "react-icons/bs";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { BsSun } from "react-icons/bs";
 import info from "./info.json";
 import {
   AiFillTwitterCircle,
   AiFillLinkedin,
   AiFillYoutube,
 } from "react-icons/ai";
+import { useState } from "react";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
   return (
-    <div>
-      <main className="bg-white px-10">
+    <div className={darkMode ? "dark" : ""}>
+      <main className="bg-white px-10 dark:bg-gray-900 dark:text-white">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between">
             <h1 className="text-xl font-marker">Developedby{info.fname}</h1>
             <ul className="flex items-center">
               <li>
-                <BsFillMoonStarsFill className="cursor-pointer text-2xl" />
+                {!darkMode && (
+                  <FaMoon
+                    onClick={() => {
+                      setDarkMode(!darkMode);
+                      console.log(
+                        "🚀 ~ file: App.js:25 ~ App ~ prev",
+                        darkMode
+                      );
+                    }}
+                    className="cursor-pointer text-2xl"
+                  />
+                )}
+                {darkMode && (
+                  <FaSun
+                    onClick={() => {
+                      setDarkMode(!darkMode);
+                      console.log(
+                        "🚀 ~ file: App.js:34 ~ App ~ prev",
+                        darkMode
+                      );
+                    }}
+                    className="cursor-pointer text-2xl dark:text-white"
+                  />
+                )}
               </li>
             </ul>
           </nav>
@@ -25,11 +51,11 @@ function App() {
               {info.name}
             </h2>
             <h3 className="text-xl py-2">{info.designation}</h3>
-            <p className="text-md py-5 leading-8 text-gray-800">
+            <p className="text-md py-5 leading-8 text-gray-800 dark:text-white">
               {info.summary}
             </p>
           </div>
-          <div className="text-5xl flex justify-center gap-16 text-gray-600">
+          <div className="text-5xl flex justify-center gap-16 text-gray-600 dark:text-white">
             <AiFillTwitterCircle />
             <AiFillLinkedin />
             <AiFillYoutube />
